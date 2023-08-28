fd04c4332d60906bd09771e478cedb9c40153bc5
4yejw <eo4yejw@naver.com>
2023-08-28 14:14:20 +0900

    commit

---
+++ Sort/bubble_sort.cpp
@@ -0,0 +1,19 @@
+//bubble sort O(n^2)
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define SIZE 9
+void BubbleSort(int A[]) {
+	for (int pass = 1; pass <= SIZE - 1; pass++) {
+		for (int i = 1; i <= SIZE - pass; i++) {
+			if (A[i - 1] > A[i]) 
+				swap(A[i - 1], A[i]);
+		}
+	}
+}
+int main() {
+	int A[SIZE] = {90,50,40,20,10,30,60,80,70};
+	BubbleSort(A);
+	for (int i = 0; i < 9; i++) 
+		cout << A[i] << ' ';
+}
